e728a226c688310d171469f23a768ef7e7991e20
Silas Bartha <silas@exvacuum.dev>
2023-11-04 14:19:19 -0400

    nvim | lua ls + symbol telescope bind

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
 -- Plugins
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -15,21 +15,21 @@ local packer_bootstrap = ensure_packer()
 local has_local_setup, local_setup = pcall(require, "local/init")
 
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
-  use {
-	'goolord/alpha-nvim',
-	config = function() require('alpha').setup(require('plugin/alpha-theme').config) end
-  }
-
-  use {
-	'xiyaowong/transparent.nvim',
-	config = function() require('transparent').setup() end
-  }
+	use 'wbthomason/packer.nvim'
 
 	use {
-	    "nvim-telescope/telescope-file-browser.nvim",
-	    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" }
+		'goolord/alpha-nvim',
+		config = function() require('alpha').setup(require('plugin/alpha-theme').config) end
+	}
+
+	use {
+		'xiyaowong/transparent.nvim',
+		config = function() require('transparent').setup() end
+	}
+
+	use {
+		"nvim-telescope/telescope-file-browser.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" }
 	}
 	use {
 		'neovim/nvim-lspconfig',
@@ -51,7 +51,7 @@ require('packer').startup(function(use)
 	}
 
 
-	use { 
+	use {
 		"nvim-treesitter/nvim-treesitter",
 		config = function() require('plugin/treesitter') end
 	}
@@ -61,10 +61,10 @@ require('packer').startup(function(use)
 		config = function() require('plugin/lualine') end
 	}
 	if has_local_setup then local_setup.packer(use) end
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end) 
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
 
 -- Options
 vim.o.number = 1
@@ -74,8 +74,9 @@ vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
 -- Keybinds
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope file_browser<CR>', opts)
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -87,32 +88,32 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local keybind_opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, keybind_opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keybind_opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, keybind_opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keybind_opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, keybind_opts)
+		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, keybind_opts)
+		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, keybind_opts)
+		vim.keymap.set('n', '<leader>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, keybind_opts)
+		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, keybind_opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, keybind_opts)
+		vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, keybind_opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, keybind_opts)
+		vim.keymap.set('n', '<leader>f', function()
+			vim.lsp.buf.format { async = true }
+		end, keybind_opts)
+	end,
 })
 
-if has_local_setup then local_setup.setup(use) end
+if has_local_setup then local_setup.setup() end
